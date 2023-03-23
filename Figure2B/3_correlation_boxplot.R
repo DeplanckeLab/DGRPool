6ad89a3636bf9681e6be0e6abd00bb594dbf4334
Vincent Gardeux <vincent.gardeux@epfl.ch>
2023-03-23 11:17:29 +0100

    Update Fig2B with only curated studies

--- Figure2B/3_correlation_boxplot.R
+++ Figure2B/3_correlation_boxplot.R
@@ -2,13 +2,13 @@
 ## Project: DGRPool
 ## Script purpose: Plotting phenotype-phenotype correlation boxplot (within- and cross-studies)
 ## Version: 1.0.0
-## Date Created: 2022 Dec 12
-## Date Modified: 2023 Mar 21
+## Date Created: 2023 Mar 20
+## Date Modified: 2023 Mar 23
 ## Author: Vincent Gardeux (vincent.gardeux@epfl.ch)
 ##################################################
 
 ## Working directory
-setwd("/data/gardeux/DGRPool/")
+setwd("DGRPool/")
 
 # Libraries
 suppressPackageStartupMessages(library(data.table))
@@ -30,27 +30,47 @@ samples_ordered <- colnames(data.correlation_spearman) # It is ordered by the pr
 data.annot_studies <- data.frame(Study = sample_studies)
 rownames(data.annot_studies) <- samples_ordered
 curated_studies <- subset(data.annot_studies, Study %in% paste0("S", 1:41))
+curated_studies <- unique(curated_studies$Study)
 
 # Values for within- and cross-study correlations
-results <- data.frame(matrix(nrow = (nrow(data.correlation_spearman)*nrow(data.correlation_spearman) - nrow(data.correlation_spearman))/2, ncol = 2))
-colnames(results) <- c("Value", "Type")
-n <- 1
-pb <- txtProgressBar(min = 1, max = nrow(results), style = 3, width = 50, char = "=")
-for(i in 1:(nrow(data.correlation_spearman) - 1)){
-	for(j in (i+1):nrow(data.correlation_spearman)){
-		if(sample_studies[i] == sample_studies[j]){
-			results[n, ] <- c(data.correlation_spearman[i, j], "Within-study")
-		} else {
-			results[n, ] <- c(data.correlation_spearman[i, j], "Cross-study")
-		}
-		n <- n + 1
-		setTxtProgressBar(pb, n)
-	}
-}
+# results <- data.frame(matrix(nrow = (nrow(data.correlation_spearman)*nrow(data.correlation_spearman) - nrow(data.correlation_spearman))/2, ncol = 2))
+# colnames(results) <- c("Value", "Type")
+# results$Curated <- F
+# n <- 1
+# pb <- txtProgressBar(min = 1, max = nrow(results), style = 3, width = 50, char = "=")
+# for(i in 1:(nrow(data.correlation_spearman) - 1)){
+# 	for(j in (i+1):nrow(data.correlation_spearman)){
+# 		results[n, "Value"] <- data.correlation_spearman[i, j]
+# 		if(sample_studies[i] == sample_studies[j]){
+# 			results[n, "Type"] <- "Within-study"
+# 			if(sample_studies[i] %in% curated_studies) results[n, "Curated"] <- T
+# 		} else {
+# 			results[n, "Type"] <- "Cross-study"
+# 			if(sample_studies[i] %in% curated_studies) {
+# 				if(sample_studies[j] %in% curated_studies) results[n, "Curated"] <- T
+# 			}
+# 		}
+# 		n <- n + 1
+# 		setTxtProgressBar(pb, n)
+# 	}
+# }
+# saveRDS(results, file = "correlation.boxplot.result.rds")
+results <- readRDS(file = "correlation.boxplot.result.rds")
 
 # Barplot
 results$Value <- abs(as.numeric(results$Value))
 results <- subset(results, !is.na(Value))
+data.sum <- summarise(group_by(results, Type), MD = median(Value), MN = mean(Value))
+p <- ggplot(results, aes(x = Type, y = Value, fill = Type)) + geom_boxplot(show.legend = FALSE, width=0.5) + theme_classic() + scale_fill_manual(values = c("#8ba8c8", "#E69A8DFF"))
+p <- p + stat_compare_means(method = "wilcox.test", paired = F, comparisons = list( c("Cross-study", "Within-study")))
+p <- p + geom_text(data = data.sum, aes(Type, MD, label = round(MD,3)), position = position_dodge(width = 0.8), size = 3, vjust = -0.5, color = "black")
+p <- p + xlab("") + ylab("Spearman's correlation")
+ggsave(p, filename = "boxplot.within.cross.study.pdf", width = 3, height = 4)
+ggsave(p, filename = "boxplot.within.cross.study.png", width = 3, height = 4)
+message("Displayed values are median. Mean values are ", round(subset(data.sum, Type == "Cross-study")$MN, 3) , " for cross-study, and ", round(subset(data.sum, Type == "Within-study")$MN, 3), " for Within-study")
+
+# Barplot (curated)
+results <- subset(results, Curated == T)
 data.sum <- summarise(group_by(results, Type), MD = median(Value), MN = mean(Value))
 p <- ggplot(results, aes(x = Type, y = Value, fill = Type)) + geom_boxplot(show.legend = FALSE, width=0.5) + theme_classic() + scale_fill_manual(values = c("#8ba8c8", "#E69A8DFF"))
 p <- p + stat_compare_means(method = "wilcox.test", paired = F, comparisons = list( c("Cross-study", "Within-study")))
