142a8905f3ceb82041ffc364343c27c577d1c8f5
Vincent Gardeux <vincent.gardeux@epfl.ch>
2023-04-03 14:43:28 +0200

    Update pdf

--- Figure1A/pubmed_barplot.R
+++ Figure1A/pubmed_barplot.R
@@ -18,7 +18,5 @@ suppressPackageStartupMessages(library(ggplot2))
 data.pubmed <- fread("PubMed_Timeline_Results_by_Year.csv", sep = ",", header = T, data.table = F)
 	
 # Plot
-p <- ggplot(data.pubmed, aes(x = as.character(Year), y = Count)) + geom_col() + ylab("Number of publications per year") + xlab("") + theme(axis.line.y = element_line(size = 0.3, colour = "black"), axis.line.x = element_line(size = 0.3, colour = "black"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_text(angle = 90, size = 10))
-p 
-
+p <- ggplot(data.pubmed, aes(x = as.character(Year), y = Count)) + geom_col() + ylab("Number of publications per year") + xlab("") + theme(axis.line.y = element_line(linewidth = 0.3, colour = "black"), axis.line.x = element_line(linewidth = 0.3, colour = "black"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_text(angle = 90, size = 10))
 ggsave(plot = p, filename = "Pubmed.plot.pdf", width = 3, height = 5)
