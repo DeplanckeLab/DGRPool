ca91ed80baf21c9673faa010a4a51d3de570087d
Vincent Gardeux <gardeux@updeplasrv6.epfl.ch>
2024-07-22 13:00:44 +0200

    Update 2B

--- Figure_2B/correlation_boxplot.R
+++ Figure_2B/correlation_boxplot.R
@@ -26,45 +26,38 @@ studies_ordered <- unique(sample_studies) # It is ordered by the previous script
 samples_ordered <- colnames(data.correlation_spearman) # It is ordered by the previous script
 data.annot_studies <- data.frame(Study = sample_studies)
 rownames(data.annot_studies) <- samples_ordered
-curated_studies <- subset(data.annot_studies, Study %in% paste0("S", 1:41))
-curated_studies <- unique(curated_studies$Study)
+
+## Find curated studies
+phenotypes_information <- fread("../resources/phenotypes.tsv", data.table = F)
+phenotypes_information <- phenotypes_information[,c("study_id", "study_name", "study_status")]
+phenotypes_information <- phenotypes_information[!duplicated(phenotypes_information),]
+phenotypes_information$study_name <- paste0("(",phenotypes_information$study_name,")")
+rownames(phenotypes_information) <- paste0("S", phenotypes_information$study_id)
+curated_studies <- paste0("S", sort(phenotypes_information$study_id[phenotypes_information$study_status == "integrated"]))
+phenotypes_information <- phenotypes_information[curated_studies,]
 
 # Values for within- and cross-study correlations
-# results <- data.frame(matrix(nrow = (nrow(data.correlation_spearman)*nrow(data.correlation_spearman) - nrow(data.correlation_spearman))/2, ncol = 2))
-# colnames(results) <- c("Value", "Type")
-# results$Curated <- F
-# n <- 1
-# pb <- txtProgressBar(min = 1, max = nrow(results), style = 3, width = 50, char = "=")
-# for(i in 1:(nrow(data.correlation_spearman) - 1)){
-# 	for(j in (i+1):nrow(data.correlation_spearman)){
-# 		results[n, "Value"] <- data.correlation_spearman[i, j]
-# 		if(sample_studies[i] == sample_studies[j]){
-# 			results[n, "Type"] <- "Within-study"
-# 			if(sample_studies[i] %in% curated_studies) results[n, "Curated"] <- T
-# 		} else {
-# 			results[n, "Type"] <- "Cross-study"
-# 			if(sample_studies[i] %in% curated_studies) {
-# 				if(sample_studies[j] %in% curated_studies) results[n, "Curated"] <- T
-# 			}
-# 		}
-# 		n <- n + 1
-# 		setTxtProgressBar(pb, n)
-# 	}
-# }
-# saveRDS(results, file = "correlation.boxplot.result.rds")
-results <- readRDS(file = "correlation.boxplot.result.rds")
-
-# Barplot
-results$Value <- abs(as.numeric(results$Value))
-results <- subset(results, !is.na(Value))
-data.sum <- summarise(group_by(results, Type), MD = median(Value), MN = mean(Value))
-p <- ggplot(results, aes(x = Type, y = Value, fill = Type)) + geom_boxplot(show.legend = FALSE, width=0.5) + theme_classic() + scale_fill_manual(values = c("#8ba8c8", "#E69A8DFF"))
-p <- p + stat_compare_means(method = "wilcox.test", paired = F, comparisons = list( c("Cross-study", "Within-study")))
-p <- p + geom_text(data = data.sum, aes(Type, MD, label = round(MD,3)), position = position_dodge(width = 0.8), size = 3, vjust = -0.5, color = "black")
-p <- p + xlab("") + ylab("Spearman's correlation")
-ggsave(p, filename = "boxplot.within.cross.study.pdf", width = 3, height = 4)
-ggsave(p, filename = "boxplot.within.cross.study.png", width = 3, height = 4)
-message("Displayed values are median. Mean values are ", round(subset(data.sum, Type == "Cross-study")$MN, 3) , " for cross-study, and ", round(subset(data.sum, Type == "Within-study")$MN, 3), " for Within-study")
+results <- data.frame(matrix(nrow = (nrow(data.correlation_spearman)*nrow(data.correlation_spearman) - nrow(data.correlation_spearman))/2, ncol = 2))
+colnames(results) <- c("Value", "Type")
+results$Curated <- F
+n <- 1
+pb <- txtProgressBar(min = 1, max = nrow(results), style = 3, width = 50, char = "=")
+for(i in 1:(nrow(data.correlation_spearman) - 1)){
+	for(j in (i+1):nrow(data.correlation_spearman)){
+		results[n, "Value"] <- data.correlation_spearman[i, j]
+		if(sample_studies[i] == sample_studies[j]){
+			results[n, "Type"] <- "Within-study"
+			if(sample_studies[i] %in% curated_studies) results[n, "Curated"] <- T
+		} else {
+			results[n, "Type"] <- "Cross-study"
+			if(sample_studies[i] %in% curated_studies) {
+				if(sample_studies[j] %in% curated_studies) results[n, "Curated"] <- T
+			}
+		}
+		n <- n + 1
+		setTxtProgressBar(pb, n)
+	}
+}
 
 # Barplot (curated)
 results <- subset(results, Curated == T)
@@ -74,5 +67,5 @@ p <- p + stat_compare_means(method = "wilcox.test", paired = F, comparisons = li
 p <- p + geom_text(data = data.sum, aes(Type, MD, label = round(MD,3)), position = position_dodge(width = 0.8), size = 3, vjust = -0.5, color = "black")
 p <- p + xlab("") + ylab("Spearman's correlation")
 ggsave(p, filename = "boxplot.within.cross.study.pdf", width = 3, height = 4)
-ggsave(p, filename = "boxplot.within.cross.study.png", width = 3, height = 4)
+#ggsave(p, filename = "boxplot.within.cross.study.png", width = 3, height = 4)
 message("Displayed values are median. Mean values are ", round(subset(data.sum, Type == "Cross-study")$MN, 3) , " for cross-study, and ", round(subset(data.sum, Type == "Within-study")$MN, 3), " for Within-study")
